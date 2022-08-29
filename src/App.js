2ea7b91dc90e98a694731801f7a11c819184470c
mani1911 <mail2aj19@gmail.com>
2022-08-29 21:48:16 +0530

    made basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import {Box} from '@mui/material';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import {Navbar, VideoDetail, ChannelDetail, SearchFeed, Feed} from './components'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Box sx = {{backgroundColor : '#000'}}>
+      <Navbar/>
+      <Routes>
+        <Route path = "/" element = {<Feed/>}></Route>
+        <Route path = "/video/:id" element = {<VideoDetail/>}></Route>
+        <Route path = "/channel/:id" element = {<ChannelDetail/>}></Route>
+        <Route path = "/search/:searchTerm" element = {<SearchFeed/>}></Route>
+      </Routes>
+
+      </Box>
+    </BrowserRouter>
   );
 }
 
